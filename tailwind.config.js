1fee41f8e54f27734c199dcd5587f3dfb9f2149c
andreparmar <88522804+andreparmar@users.noreply.github.com>
2022-10-13 21:53:25 -0400

    push to hostinger

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./*.html"],
   theme: {
     fontFamily: {
       display: ["Clash Display", "sans-serif"],
